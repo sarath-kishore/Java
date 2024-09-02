076b77a2bf23e371af7f9fc1be4623574b8af133
sarath-kishore <161718220+sarath-kishore@users.noreply.github.com>
2024-09-02 14:36:09 +0530

    Implemented Prototype design pattern in an  example

---
+++ Design Patterns/src/Prototype/Graphic.java
@@ -0,0 +1,20 @@
+package Prototype;
+
+public abstract class Graphic {
+
+    private int heightInPixels;
+
+    public Graphic(int heightInPixels) {
+        this.heightInPixels = heightInPixels;
+    }
+
+    public int getHeightInPixels() {
+        return heightInPixels;
+    }
+
+    public void setHeightInPixels(int heightInPixels) {
+        this.heightInPixels = heightInPixels;
+    }
+
+    public abstract Graphic clone();
+}
