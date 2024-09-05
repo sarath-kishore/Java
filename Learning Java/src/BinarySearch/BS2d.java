1d4e813c9d8665214a3a94da214339ed5f5e4291
sarath-kishore <161718220+sarath-kishore@users.noreply.github.com>
2024-09-05 20:50:47 +0530

    Corrected the code for 2d Binary Search

--- Learning Java/src/BinarySearch/BS2d.java
+++ Learning Java/src/BinarySearch/BS2d.java
@@ -23,8 +23,8 @@ public class BS2d {
                 {23,30,34,50}
         };
 
-        System.out.println("not strictly sorted element index: " + Arrays.toString(bs2d(arrAll, 51)));
-        System.out.println("strictly sorted element index: " + Arrays.toString(bs2dStrictlySorted(arrSortedStrictly, 10)));
+        System.out.println("not strictly sorted element index: " + Arrays.toString(bs2d(arrAll, 37)));
+        System.out.println("strictly sorted element index: " + Arrays.toString(bs2dStrictlySorted(arrSortedStrictly, 23)));
     }
 
     static int[] binarySearch1d(int[][] matrix, int target, int row, int cStart, int cEnd){
@@ -68,33 +68,44 @@ public class BS2d {
             }
         }
 
-//      { check the middle column elements of the 2 remaining rows.
-        if(matrix[rStart][midCol]==target)
-            return new int[]{rStart, midCol};
+// method 1:
+        int[] temp = binarySearch1d(matrix, target, rStart, 0, cEnd);
+        if(temp[0] == -1)
+            temp = binarySearch1d(matrix, target, rStart+1, 0, cEnd);
 
-        if(matrix[rStart+1][midCol]==target)
-            return new int[]{rStart+1, midCol};
-//      }
+        return temp;
+// method 1 ----------*
 
+// method 2:
+////      { check the middle column elements of the 2 remaining rows.
+//        if(matrix[rStart][midCol]==target)
+//            return new int[]{rStart, midCol};
+//
+//        if(matrix[rStart+1][midCol]==target)
+//            return new int[]{rStart+1, midCol};
+////      }
+//
+//
+//// check the left side elements of 1st row middle column
+//        if(target<=matrix[rStart][midCol-1])
+//            return binarySearch1d(matrix, target, rStart, 0, midCol-1);
+//
+//// check the right side elements of 1st row middle column
+//        else if(target>=matrix[rStart][midCol+1] && target<=matrix[rStart][cEnd])
+//            return binarySearch1d(matrix, target, rStart, midCol+1, cEnd);
+//
+//// check the left side elements of 2nd row middle column
+//        else if(target<=matrix[rStart+1][midCol-1])
+//            return binarySearch1d(matrix, target, rStart+1, 0, midCol-1);
+//
+//// check the right side elements of 2nd row middle column
+//        else
+//            return binarySearch1d(matrix, target, rStart+1, midCol+1, cEnd);
+//
+////        return new int[]{-1,-1}; no need for this as it will never be reached.
+////        if no IF statements are evaluated, the last else block will execute and that result will be returned.
 
-// check the left side elements of 1st row middle column
-        if(target<=matrix[rStart][midCol-1])
-            return binarySearch1d(matrix, target, rStart, 0, midCol-1);
-
-// check the right side elements of 1st row middle column
-        else if(target>=matrix[rStart][midCol+1] && target<=matrix[rStart][cEnd])
-            return binarySearch1d(matrix, target, rStart, midCol+1, cEnd);
-
-// check the left side elements of 2nd row middle column
-        else if(target<=matrix[rStart+1][midCol-1])
-            return binarySearch1d(matrix, target, rStart+1, 0, midCol-1);
-
-// check the right side elements of 2nd row middle column
-        else
-            return binarySearch1d(matrix, target, rStart+1, midCol+1, cEnd);
-
-//        return new int[]{-1,-1}; no need for this as it will never be reached.
-//        if no IF statements are evaluated, the last else block will execute and that result will be returned.
+// method 2 ----------*
     }
 
     static int[] bs2d(int[][] arr, int target){
@@ -102,19 +113,29 @@ public class BS2d {
         int col_low = 0;
         int col_high = arr[0].length-1;
         int row_high = arr.length-1; // row count
-//        int n = arr[0].length; // col count
 
-        while(row_low<=row_high && col_high>=0){
-            int mid_row = (row_low+row_high)/2;
-            int mid_col = (col_low+col_high)/2;
-            if(arr[mid_row][mid_col]==target){
-                return new int[]{mid_row, mid_col};
-            }else if(arr[mid_row][mid_col]<target){
-                row_low++;
-            }else{
-                row_low++;
+//        while(row_low<=row_high && col_high>=0){
+//            //starts from 0, c-1
+//            if(arr[row_low][col_high]==target){
+//                return new int[]{row_low, col_high};
+//            }else if(arr[row_low][col_high] < target){
+//                row_low = row_low+1;
+//            }else if(arr[row_low][col_high] > target){
+//                col_high = col_high-1;
+//            }
+//        }
+
+        while(row_high>=0 && col_low <= col_high){
+            //starts from r-1, 0
+            if(arr[row_high][col_low]==target){
+                return new int[]{row_high, col_low};
+            }else if(arr[row_high][col_low] < target){
+                col_low = col_low+1;
+            }else if(arr[row_high][col_low] > target){
+                row_high = row_high-1;
             }
         }
+
         return new int[]{-1,-1};
     }
 }
